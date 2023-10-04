0511283750d54e5521e7070c875ec451f3900075
John Doe <tirunaharipandu@gmail.com>
2023-10-04 13:45:29 +0530

    Google maps intergration

--- lib/maps/map_screen.dart
+++ lib/maps/map_screen.dart
@@ -1,41 +1,110 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
-  const MapScreen({super.key});
+  const MapScreen({Key? key}) : super(key: key);
 
   @override
   State<MapScreen> createState() => _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
-  late GoogleMapController mapController;
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
 
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+  CameraPosition _buildCameraPosition() {
+    if (latitude != null && longitude != null) {
+      return CameraPosition(
+        target: LatLng(latitude!, longitude!),
+        zoom: 14.4746,
+      );
+    } else {
+      return _kGooglePlex; // Use the default camera position if location is not available yet
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  double? latitude;
+  double? longitude;
+  Set<Marker> markers = {};
+  String? selectedMarkerCoordinates; // Store selected marker coordinates
+
+  // Function to handle map tap and add a marker
+  void _onMapTapped(LatLng latLng) {
+    setState(() {
+      markers.clear(); // Clear previous markers (if any)
+      markers.add(
+        Marker(
+          markerId: MarkerId("user_location"),
+          position: latLng,
+          infoWindow: InfoWindow(
+            title: "Marked Location",
+            snippet: "Lat: ${latLng.latitude}, Lng: ${latLng.longitude}",
+          ),
+        ),
+      );
+      selectedMarkerCoordinates =
+          "Lat: ${latLng.latitude}, Lng: ${latLng.longitude}";
+    });
+    print(latLng);
+  }
+
+  Future<void> getLocation() async {
+    try {
+      LocationPermission permission;
+      permission = await Geolocator.requestPermission();
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.best);
+      setState(() {
+        latitude = position.latitude;
+        longitude = position.longitude;
+      });
+    } catch (e) {
+      print("Error getting location: $e");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.green[700],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Map Screen"),
+        backgroundColor: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Maps Sample App'),
-          elevation: 2,
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
+      body: Column(
+        children: [
+          Container(
+            height: 300, // Reduce the map height
+            child: GoogleMap(
+              mapType: MapType.hybrid,
+              initialCameraPosition: _buildCameraPosition(),
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+              markers: markers,
+              onTap: _onMapTapped,
+            ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              selectedMarkerCoordinates ?? "Mark a location on the map",
+              style: TextStyle(fontSize: 16.0),
+            ),
+          ),
+        ],
       ),
     );
   }
